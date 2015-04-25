d414fb61645399ca4322bed35cf6612a4e0177b7
Daniel Jones <dcjones@cs.washington.edu>
2015-04-25 10:55:23 -0700

    BED parser.

--- src/intervals/intervals.jl
+++ src/intervals/intervals.jl
@@ -1,22 +1,28 @@
 
 module Intervals
 
-import Base: show, isless, push!, intersect, start, next, done, length
+import Base: show, isless, push!, intersect, start, next, done, length,
+             convert, read, read!
+using Base.Intrinsics, Compat, Color, Docile, IntervalTrees
 import Iterators
-using Base.Intrinsics
 #using DataStructures
 using Docile
 using Docile: @doc, @doc_str
 using IntervalTrees
 
-export Strand, Interval, IntervalCollection,
+import Bio: FileFormat
+
+export Strand, Interval, IntervalCollection, IntervalStream,
        STRAND_NA, STRAND_POS, STRAND_NEG, STRAND_BOTH,
-       isoverlapping
+       isoverlapping, BED
 
 include("interval.jl")
 include("stream_buffer.jl")
-include("intervalcollection.jl")
 include("intervalstream.jl")
+include("intervalcollection.jl")
+
+# Parsing file types
+include("bed.jl")
 
 
 end
